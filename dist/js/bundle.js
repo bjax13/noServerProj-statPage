adb91305fd63ed8c1cff731e963d1159c89da80f
Bryan Jackson <bjack13@gmail.com>
2016-12-23 10:06:44 -0700

    added some information and working on backgroundColor

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -25,6 +25,39 @@ angular.module('statApp').controller('homeCtrl', function ($scope) {
 });
 'use strict';
 
+angular.module('statApp').service('codewarsSvc', function ($http, $q) {
+
+  this.coderInfo = function () {
+    var defer = $q.defer();
+    $http({
+      method: 'GET',
+      url: 'https://www.codewars.com/api/v1/users/bjack13'
+      // DA2K-3FnsohzhzAp7xvQ codewars auth key
+    }).then(function (response) {
+      console.log(response.data);
+      var dataObj = {
+        userName: response.data.username,
+        summaryImg: 'https://www.codewars.com/users/' + response.data.username + '/badges/large',
+        clan: response.data.clan,
+        honor: response.data.honor,
+        leaderboardPosition: response.data.leaderboardPosition,
+        overallRank: response.data.ranks.overall,
+        skills: response.data.skills,
+        challangeCompleted: response.data.codeChallenges.totalCompleted
+      };
+      console.log(dataObj.skills);
+      if (dataObj.skills === null) {
+        dataObj.skills = 'No skills displayed on CodeWars.com';
+      }
+
+      defer.resolve(dataObj);
+    });
+
+    return defer.promise;
+  };
+});
+'use strict';
+
 angular.module('statApp').controller('soloStatCtrl', function ($scope, codewarsSvc) {
   $scope.test = "It is alive!!";
 
@@ -36,45 +69,6 @@ angular.module('statApp').controller('soloStatCtrl', function ($scope, codewarsS
   };
 
   $scope.coderInfo();
-});
-'use strict';
-
-angular.module('statApp').service('codewarsSvc', function ($http, $q) {
-
-  this.coderInfo = function () {
-    var defer = $q.defer();
-    $http({
-      method: 'GET',
-      url: 'https://www.codewars.com/api/v1/users/bjack13'
-      // DA2K-3FnsohzhzAp7xvQ codewars auth key
-    }).then(function (response) {
-      console.log(response.data);
-      defer.resolve({
-        userName: response.data.username,
-        summaryImg: 'https://www.codewars.com/users/' + response.data.username + '/badges/large',
-        clan: response.data.clan,
-        honor: response.data.honor,
-        leaderboardPosition: response.data.leaderboardPosition,
-        overallRank: response.data.ranks.overall,
-        skills: response.data.skills,
-        challangeCompleted: response.data.codeChallenges.totalCompleted
-      });
-    });
-
-    return defer.promise;
-  };
-
-  // this.getLuke = function () {
-  //     var defer = $q.defer();
-  //     $http({
-  //       method: 'GET',
-  //       url: baseUrl + 'api/people/1'
-  //     }).then(function (response) {
-  //       defer.resolve(response.data);
-  //
-  //     })
-  //     return defer.promise;
-  //   }
 });
 'use strict';
 
--- pages/services/codewarsSvc.js
+++ pages/services/codewarsSvc.js
@@ -9,7 +9,7 @@ angular.module('statApp')
         // DA2K-3FnsohzhzAp7xvQ codewars auth key
       }).then(function (response) {
         console.log(response.data);
-        defer.resolve({
+        var dataObj = {
           userName: response.data.username,
           summaryImg:'https://www.codewars.com/users/'+ response.data.username +'/badges/large',
           clan: response.data.clan,
@@ -18,23 +18,20 @@ angular.module('statApp')
           overallRank: response.data.ranks.overall,
           skills: response.data.skills,
           challangeCompleted: response.data.codeChallenges.totalCompleted,
-        })
+        }
+        console.log(dataObj.skills);
+        if (dataObj.skills === null) {
+          dataObj.skills = 'No skills displayed on CodeWars.com'
+        }
+
+
+        defer.resolve(dataObj)
       })
 
       return defer.promise;
     }
 
 
-    // this.getLuke = function () {
-    //     var defer = $q.defer();
-    //     $http({
-    //       method: 'GET',
-    //       url: baseUrl + 'api/people/1'
-    //     }).then(function (response) {
-    //       defer.resolve(response.data);
-    //
-    //     })
-    //     return defer.promise;
-    //   }
+
 
   })
